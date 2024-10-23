a62f9277db68975f15c3fca96d4aedb9ee37f250
Suling <sulingchen0123@gmail.com>
2024-10-23 14:05:04 +0800

    [UPDATE] test code

---
+++ Tests/FileStorageContextTests/UploadFile/UploadFileToGcsTests.swift
@@ -0,0 +1,15 @@
+@testable import UploadFile
+import XCTest
+
+
+final class UploadFileToGcsAPITests: XCTestCase {
+    
+    func testUploadFileToGcs() async throws {
+        let quotingCaseId = UUID().uuidString
+        let bytes = [UInt8](repeating: 0, count: 1024)
+
+        let apiHandler = APIHandler()
+        let response = try await apiHandler.uploadFile(path: .init(quotingCaseId: quotingCaseId), headers: .init(), body: .pdf(.init(bytes)))
+        XCTAssertNotNil(try response.ok.body.json.mediaLink)
+    }
+}
