a07186e8f2ea7467ca940b545e084f25b62f5a1c
Suling <sulingchen0123@gmail.com>
2024-10-23 17:08:25 +0800

    [UPDATE] rename target fileUploader to GoogleCloudStorage

--- Tests/FileStorageContextTests/GoogleCloudStorage/GcsUploaderTests.swift
+++ Tests/FileStorageContextTests/GoogleCloudStorage/GcsUploaderTests.swift
@@ -1,19 +1,19 @@
-@testable import FileUploader
+@testable import GoogleCloudStorage
 import XCTest
 
 
-final class GcsUploaderAPITests: XCTestCase {
+final class UploadReplyFormAPITests: XCTestCase {
 
     override func setUp() {
         setenv("GCS_CREDENTIALSFILE", "/home/suling/.jw_loader/credentials/gs/ai-jiabao-com-jw_storage.json", 1)
     }
     
-    func testUploadFileToGcs() async throws {
+    func testUploadReplyFormToGcs() async throws {
         let quotingCaseId = UUID().uuidString
         let bytes = [UInt8](repeating: 0, count: 1024)
 
         let apiHandler = APIHandler()
-        let response = try await apiHandler.uploadFile(path: .init(quotingCaseId: quotingCaseId), headers: .init(), body: .pdf(.init(bytes)))
+        let response = try await apiHandler.uploadReplyForm(path: .init(quotingCaseId: quotingCaseId), headers: .init(), body: .pdf(.init(bytes)))
         XCTAssertNotNil(try response.ok.body.json.mediaLink)
     }
 }
