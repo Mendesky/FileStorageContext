38d7054e8c04b5c12fa5de101876944df76b5daa
suling <sulingchen0123@gmail.com>
2025-07-16 10:25:47 +0800

    [UPDATE] adjust storage path, use typealias replace Uploader to GSCUploader

--- Sources/GoogleCloudStorage/Adapter/StoragePathGenerator.swift
+++ Sources/GoogleCloudStorage/Adapter/StoragePathGenerator.swift
@@ -3,14 +3,14 @@ import FileStorageCore
 package struct StoragePathGenerator {
     
     static func generateQuotingContextPath(quotingCaseId: String, documentId: String, ext: String) -> String {
-        return "QuotingCase\(quotingCaseId)/Quotation/\(documentId).\(ext)"
+        return "QuotingCase/\(quotingCaseId)/Quotation/\(documentId).\(ext)"
     }
     
     static func generateAuditContextPath(customerId: String, documentType: DocumentType, documentId: String, ext: String) -> String {
-        return "Customer\(customerId)/Audit/\(documentType.rawValue)/\(documentId).\(ext)"
+        return "Customer/\(customerId)/Audit/\(documentType.rawValue)/\(documentId).\(ext)"
     }
     
     static func generateCustomerRelationshipContextPath(customerId: String, documentType: DocumentType, documentId: String, ext: String) -> String {
-        return "Customer\(customerId)/CustomerRelationship/\(documentType.rawValue)/\(documentId).\(ext)"
+        return "Customer/\(customerId)/CustomerRelationship/\(documentType.rawValue)/\(documentId).\(ext)"
     }
 }
