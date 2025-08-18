6436354fe5a54bb87c21fc6ad7c730e17abff102
Grady Zhuo <gradyzhuo@jwcpas.com.tw>
2025-08-18 15:30:35 +0800

    [UPDATE] fix StorageProtocol, add setMetadata/ getMetadata/download/markDelete

--- Sources/FileStorageCore/FileStorageCore.swift
+++ Sources/FileStorageCore/FileStorageCore.swift
@@ -10,6 +10,11 @@ import Foundation
 public protocol StorageProtocol {
     associatedtype MetadataType: Metadata
     func upload(data: Data, path: String, contentType: String, metadata: [String: Codable]?, limit: FileSizeLimit) async throws -> String?
+    
+    func setMetadata(_ metadata: [String: Codable], path: String) async throws
+    func getMetadata(path: String) async throws -> [String: Codable]?
+    func download(path: String) async throws -> Data?
+    func markDelete(path: String) async throws
 }
 
 extension StorageProtocol {
@@ -17,4 +22,16 @@ extension StorageProtocol {
         
         return try await upload(data: data, path: path, contentType: contentType, metadata: metadata.represented, limit: limit)
     }
+    
+    public func setMetadata(_ metadata: MetadataType, path: String) async throws{
+        try await setMetadata(metadata.represented, path: path)
+    }
+    
+    public func getMetadata(path: String) async throws -> MetadataType?{
+        let dictionary: [String: Codable]? = try await getMetadata(path: path)
+        return dictionary.flatMap{
+            .init(from: $0)
+        }
+    }
+    
 }
